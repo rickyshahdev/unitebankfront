40e2b7c17214344a89dc0a265996e744b68a0592
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2021-03-03 19:19:28 -0500

    front

---
+++ src/components/dashboard/PDF.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import Pdf from "react-to-pdf";
+//
+//
+// const ref = React.createRef();
+//
+// const PDF = (props) => {
+//   return(
+//   <>
+//      <div className="Post" ref={ref}>
+//
+//      </div>
+//      <Pdf targetRef={ref} filename="post.pdf">
+//      {({ toPdf})} => <button onClick={toPdf}>Download Transactions
+//      </button>
+//      </Pdf>
+//     </>
+//    )
+// }
+//
+//
+// export default PDF;
